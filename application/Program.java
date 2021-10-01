7e2f8b2c1c0000dc38df2688ace4fb353911a74b
welton82 <79199144+welton82@users.noreply.github.com>
2021-10-01 08:01:14 -0300

    Add files via upload

---
+++ application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import entities.Produto;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Program {
+   public static void main(String[] args) {
+
+       List<Produto> lista = new ArrayList<>();
+
+       lista.add(new Produto("Notebook",1500.60));
+       lista.add(new Produto("fone de ouvido",50.00));
+       lista.add(new Produto("Tv 29 Lg",3650.54));
+       lista.add(new Produto("Armario Itatiaia",2506.30));
+
+       //FUNÇÃO LAMBDA INLINE
+       //  . CRIANDO UMA FUNÇAO QUE RECEBE UM PRODUTO
+
+
+
+       lista.forEach(System.out::print);
+       System.out.println();
+       lista.forEach(p -> p.setPreco(p.getPreco() * 1.1));
+       lista.forEach(System.out::print);
+
+
+    }
+}
